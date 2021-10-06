2719bbe9955a75b29a4b3f7dca8fe894ce39ba14
Rizian <ianwirawan81@gmail.com>
2021-10-07 01:41:34 +0700

    Update Triangle perimeter.py

--- AlgoProgLAB/Triangle perimeter.py
+++ AlgoProgLAB/Triangle perimeter.py
@@ -6,4 +6,4 @@ edge3 = float(input("Enter the length of edge 3: "))
 if edge1 + edge2 > edge3 and edge1 + edge3 > edge2 and edge2 + edge3 > edge1:
     print(f"The perimeter of the triangle is {edge1+edge2+edge3}!")
 else:
-    print("invalid input. Values don't represent a triangle!")
+    print("invalid input. Values don't represent a triangle and cannot be calculated!")
