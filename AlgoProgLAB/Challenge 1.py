da68a9cc648397ac589f3381775f4eb01518cbcf
Rizian <ianwirawan81@gmail.com>
2021-10-08 16:32:45 +0700

    Update Challenge 1.py

--- AlgoProgLAB/Challenge 1.py
+++ AlgoProgLAB/Challenge 1.py
@@ -1,6 +1,21 @@
 '''
 Challenge 1
 '''
+x = int(input("Enter a number: "))
+
+if x % 11 == 0:
+    print("a", end="")
+if x % 9 == 0:
+    print("b", end="")
+if x % 7 == 0:
+    print("c", end="")
+if x % 2 == 0:
+    print("d", end="")
+if (x % 11 != 0 and x % 11 != 0 and x % 9 != 0 and x % 7 != 0 and x % 2 != 0):
+    print("e", end="")
+
+'''
+could also use a list but less efficient lol
 list1 = []
 
 x = int(input("Enter a number: "))
@@ -9,7 +24,7 @@ if x % 11 == 0:
     list1.append("a")
 if x % 9 == 0:
     list1.append("b")
-if x % 5 == 0:
+if x % 7 == 0:
     list1.append("c")
 if x % 2 == 0:
     list1.append("d")
@@ -18,3 +33,4 @@ if (x % 11 != 0 and x % 11 != 0 and x % 9 != 0 and x % 7 != 0 and x % 2 != 0):
 
 n = ''.join(list1)
 print(n)
+'''
